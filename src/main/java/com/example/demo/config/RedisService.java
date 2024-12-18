ca7101038b053ac021eb8324bab292ca1c7fdfb3
admin <ggb7429@163.com>
2024-12-18 14:12:09 +0800

    demo功能描述

--- src/main/java/com/example/demo/config/RedisService.java
+++ src/main/java/com/example/demo/config/RedisService.java
@@ -22,7 +22,7 @@ public class RedisService {
     //3.查询在线用户
     //4.实现消息队列
     //5.实现分布式锁
-    //todo aop、拦截器（interceptor）、过滤器(filter)
+    //aop、拦截器（interceptor）、过滤器(filter)
 
     @Autowired
     private RedisTemplate<String,Object> redisTemplate;
